ba519c3713ab0950765e70e0bb523e3d1c431121
Anthony Tsuhako <anthony.tsuhako@navy.mil>
2015-03-31 15:13:49 -1000

    Cleaned up test code

--- src/main/java/com/spawar/app/App.java
+++ src/main/java/com/spawar/app/App.java
@@ -26,12 +26,6 @@ public class App {
 	public static void main(String[] args) {
 //		testAccumuloGraph();
 
-		try {
-			testRegex();
-		} catch (Exception e) {
-			e.printStackTrace();
-		} //end of catch
-
 		System.out.println("Hello World!");
 		}
 
@@ -51,26 +45,4 @@ public class App {
 		GraphFactory.open(cfg2);
 	} //end of testAccumuloGraph
 
-	private static void testRegex() throws Exception {
-		System.out.println("newPath: " + localizeFilepath("\\\\cci-scanctr\\Release\\Navy\\Backfile\\1149\\SSMinnow\\Backfile\\2007\\122375\\Fuel1149_12345678901234_10.pdf"));
-
-		System.out.println("newPath: " + localizeFilepath("\\\\cci-scanctr\\Release\\Navy\\Backfile\\Food\\SSMinnow\\Backfile\\2007\\122375\\FoodReceipt_987654_35400795.pdf"));
-
-		System.out.println("newPath: " + localizeFilepath("\\\\cci-scanctr\\Release\\Navy\\Backfile\\PCards\\SSMinnow\\Backfile\\2007\\122375\\PCard_Purchase Files_12345_201310_5.pdf"));
-
-		System.out.println("newPath: " + localizeFilepath("\\\\cci-scanctr\\Release\\Navy\\Backfile\\Misc\\SSMinnow\\Backfile\\2007\\122375\\FoodReceipt_VJ56852338907E_373.pdf"));
-	} //end of testRegex
-
-	private static String localizeFilepath(String networkFilepath) throws Exception {
-		String localFilepath;
-
-		try {
-			String[] pathArr = networkFilepath.split("Backfile");
-			localFilepath = "C:\\whatever\\" + pathArr[1].split("\\\\")[1] + pathArr[2];
-		} catch (Exception e) {
-			throw new Exception("Invalid filepath format <" + networkFilepath + ">");
-		} //end of catch
-
-		return localFilepath;
-	} //end of localizeFilepath
 } //end of class
