8a6c6cdc7ee111e020a58cd539048ecffd4d0cd5
atsuhako <anthony.tsuhako@navy.mil>
2015-03-31 18:09:14 -1000

    WIP: Added neo4j

--- src/main/java/com/spawar/app/App.java
+++ src/main/java/com/spawar/app/App.java
@@ -4,30 +4,36 @@ package com.spawar.app;
 //import org.apache.commons.configuration.*;
 //import com.tinkerpop.gremlin.structure.*;
 //import com.tinkerpop.gremlin.structure.util.*;
-	import com.tinkerpop.blueprints.Graph;
-	import com.tinkerpop.blueprints.GraphFactory;
-
 // 	import com.tinkerpop.gremlin.structure.Graph;
 // 	import com.tinkerpop.gremlin.structure.util.GraphFactory;
+
+	import com.tinkerpop.blueprints.Graph;
+	import com.tinkerpop.blueprints.GraphFactory;
 
 	import org.apache.commons.configuration.Configuration;
 
 	import edu.jhuapl.tinkerpop.AccumuloGraphConfiguration;
 	import edu.jhuapl.tinkerpop.AccumuloGraphConfiguration.InstanceType;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+	import java.util.regex.Matcher;
+	import java.util.regex.Pattern;
+
+	import org.neo4j.graphdb.GraphDatabaseService;
+	import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+	import org.neo4j.remote.RemoteGraphDatabase;
 
 /**
  * Hello world!
- *
+ * https://github.com/neo4j-contrib/java-rest-binding
  */
 public class App {
 	public static void main(String[] args) {
 //		testAccumuloGraph();
-
+		testNeo4j();
+		System.out.print("Press enter: ");
+		System.console().readLine();
 		System.out.println("Hello World!");
-		}
+	} //end of main
 
 	private static void testAccumuloGraph() {
 		Configuration cfg1 = new AccumuloGraphConfiguration().setInstanceType(InstanceType.Mock).setGraphName("graph");
@@ -45,4 +51,21 @@ public class App {
 		GraphFactory.open(cfg2);
 	} //end of testAccumuloGraph
 
+	private static void testNeo4j() {
+		//GraphDatabaseService graphDb = new GraphDatabaseFactory().newEmbeddedDatabase("C:/Users/Anthony/Documents/Neo4j/default.graphdb");
+		GraphDatabaseService graphDb = RemoteGraphDatabase("localhost:3131");
+		//registerShutdownHook(graphDb);
+	} //end of testNeo4j
+
+	private static void registerShutdownHook(final GraphDatabaseService graphDb) {
+		// Registers a shutdown hook for the Neo4j instance so that it
+		// shuts down nicely when the VM exits (even if you "Ctrl-C" the
+		// running application).
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			@Override
+			public void run() {
+				graphDb.shutdown();
+			}
+		});
+	} //end of registerShutdownHook
 } //end of class
