04ee63748491aa30065294034b4dc220dc6af5b4
Anthony Tsuhako <anthony.tsuhako@navy.mil>
2015-03-31 15:08:55 -1000

    Updated pom to add exec:java goal; cleaned up App.java

--- src/main/java/com/spawar/app/App.java
+++ src/main/java/com/spawar/app/App.java
@@ -10,31 +10,67 @@ package com.spawar.app;
 // 	import com.tinkerpop.gremlin.structure.Graph;
 // 	import com.tinkerpop.gremlin.structure.util.GraphFactory;
 
- 	import org.apache.commons.configuration.Configuration;
+	import org.apache.commons.configuration.Configuration;
 
-import edu.jhuapl.tinkerpop.AccumuloGraphConfiguration;
-import edu.jhuapl.tinkerpop.AccumuloGraphConfiguration.InstanceType;
+	import edu.jhuapl.tinkerpop.AccumuloGraphConfiguration;
+	import edu.jhuapl.tinkerpop.AccumuloGraphConfiguration.InstanceType;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Hello world!
  *
  */
 public class App {
-    public static void main( String[] args ) {
-    	Configuration cfg1 = new AccumuloGraphConfiguration().setInstanceType(InstanceType.Mock).setGraphName("graph");
+	public static void main(String[] args) {
+//		testAccumuloGraph();
 
-Configuration cfg2 = new AccumuloGraphConfiguration()
-  .setInstanceType(InstanceType.Distributed)
-  .setZooKeeperHosts("localhost:2181")
-  .setInstanceName("acc1")
-  .setUser("acc1_user").setPassword("password")
-  .setGraphName("graph")
-  .setCreate(true);
+		try {
+			testRegex();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} //end of catch
 
-//return GraphFactory.open(cfg);
+		System.out.println("Hello World!");
+		}
 
-			GraphFactory.open(cfg2);
+	private static void testAccumuloGraph() {
+		Configuration cfg1 = new AccumuloGraphConfiguration().setInstanceType(InstanceType.Mock).setGraphName("graph");
 
-      System.out.println( "Hello World!" );
-    }
-}
+		Configuration cfg2 = new AccumuloGraphConfiguration()
+			.setInstanceType(InstanceType.Distributed)
+			.setZooKeeperHosts("192.168.227.133:2181")
+			.setInstanceName("acc1")
+			.setUser("acc1_user").setPassword("password")
+			.setGraphName("graph")
+			.setCreate(true);
+
+		//return GraphFactory.open(cfg);
+
+		GraphFactory.open(cfg2);
+	} //end of testAccumuloGraph
+
+	private static void testRegex() throws Exception {
+		System.out.println("newPath: " + localizeFilepath("\\\\cci-scanctr\\Release\\Navy\\Backfile\\1149\\SSMinnow\\Backfile\\2007\\122375\\Fuel1149_12345678901234_10.pdf"));
+
+		System.out.println("newPath: " + localizeFilepath("\\\\cci-scanctr\\Release\\Navy\\Backfile\\Food\\SSMinnow\\Backfile\\2007\\122375\\FoodReceipt_987654_35400795.pdf"));
+
+		System.out.println("newPath: " + localizeFilepath("\\\\cci-scanctr\\Release\\Navy\\Backfile\\PCards\\SSMinnow\\Backfile\\2007\\122375\\PCard_Purchase Files_12345_201310_5.pdf"));
+
+		System.out.println("newPath: " + localizeFilepath("\\\\cci-scanctr\\Release\\Navy\\Backfile\\Misc\\SSMinnow\\Backfile\\2007\\122375\\FoodReceipt_VJ56852338907E_373.pdf"));
+	} //end of testRegex
+
+	private static String localizeFilepath(String networkFilepath) throws Exception {
+		String localFilepath;
+
+		try {
+			String[] pathArr = networkFilepath.split("Backfile");
+			localFilepath = "C:\\whatever\\" + pathArr[1].split("\\\\")[1] + pathArr[2];
+		} catch (Exception e) {
+			throw new Exception("Invalid filepath format <" + networkFilepath + ">");
+		} //end of catch
+
+		return localFilepath;
+	} //end of localizeFilepath
+} //end of class
