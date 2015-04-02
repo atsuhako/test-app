4eb4017b459593e8d69e82e74fe1e6f56dd9878c
Anthony Tsuhako <anthony.tsuhako@navy.mil>
2015-04-01 14:12:00 -1000

    Cleaned up pom and unrequired dependencies

--- src/main/java/com/spawar/app/App.java
+++ src/main/java/com/spawar/app/App.java
@@ -1,26 +1,14 @@
 package com.spawar.app;
 
-//import edu.jhuapl.tinkerpop.*;
-//import org.apache.commons.configuration.*;
-//import com.tinkerpop.gremlin.structure.*;
-//import com.tinkerpop.gremlin.structure.util.*;
-// 	import com.tinkerpop.gremlin.structure.Graph;
-// 	import com.tinkerpop.gremlin.structure.util.GraphFactory;
+import edu.jhuapl.tinkerpop.AccumuloGraph;
+import edu.jhuapl.tinkerpop.AccumuloGraphConfiguration;
+import edu.jhuapl.tinkerpop.AccumuloGraphConfiguration.InstanceType;
 
-	import com.tinkerpop.blueprints.Graph;
-	import com.tinkerpop.blueprints.GraphFactory;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-	import org.apache.commons.configuration.Configuration;
-
-	import edu.jhuapl.tinkerpop.AccumuloGraphConfiguration;
-	import edu.jhuapl.tinkerpop.AccumuloGraphConfiguration.InstanceType;
-
-	import java.util.regex.Matcher;
-	import java.util.regex.Pattern;
-
-	import org.neo4j.graphdb.GraphDatabaseService;
-	import org.neo4j.graphdb.factory.GraphDatabaseFactory;
-	import org.neo4j.remote.RemoteGraphDatabase;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 
 /**
  * Hello world!
@@ -28,33 +16,36 @@ package com.spawar.app;
  */
 public class App {
 	public static void main(String[] args) {
-//		testAccumuloGraph();
-		testNeo4j();
-		System.out.print("Press enter: ");
-		System.console().readLine();
+		testAccumuloGraph();
+//		testNeo4j();
+
 		System.out.println("Hello World!");
 	} //end of main
 
 	private static void testAccumuloGraph() {
-		Configuration cfg1 = new AccumuloGraphConfiguration().setInstanceType(InstanceType.Mock).setGraphName("graph");
+		AccumuloGraphConfiguration cfg1 = new AccumuloGraphConfiguration().setInstanceType(InstanceType.Mock).setGraphName("graph");
 
-		Configuration cfg2 = new AccumuloGraphConfiguration()
+		AccumuloGraphConfiguration cfg2 = new AccumuloGraphConfiguration()
 			.setInstanceType(InstanceType.Distributed)
 			.setZooKeeperHosts("192.168.227.133:2181")
 			.setInstanceName("acc1")
-			.setUser("acc1_user").setPassword("password")
+			.setUser("acc1_user")
+			.setPassword("password")
 			.setGraphName("graph")
 			.setCreate(true);
 
 		//return GraphFactory.open(cfg);
 
-		GraphFactory.open(cfg2);
+		//Graph graph = GraphFactory.open(cfg2);
+		AccumuloGraph graph = new AccumuloGraph(cfg2);
 	} //end of testAccumuloGraph
 
 	private static void testNeo4j() {
-		//GraphDatabaseService graphDb = new GraphDatabaseFactory().newEmbeddedDatabase("C:/Users/Anthony/Documents/Neo4j/default.graphdb");
-		GraphDatabaseService graphDb = RemoteGraphDatabase("localhost:3131");
-		//registerShutdownHook(graphDb);
+		GraphDatabaseService graphDb = new GraphDatabaseFactory().newEmbeddedDatabase("C:/Users/Anthony/Documents/Neo4j/default.graphdb");
+		registerShutdownHook(graphDb);
+
+		System.out.print("Press enter to continue: ");
+		System.console().readLine();
 	} //end of testNeo4j
 
 	private static void registerShutdownHook(final GraphDatabaseService graphDb) {
